75f62f664dc71ea6e34a6b27ae520a60b511d278
marceloShibuya <50499486+marceloShibuya@users.noreply.github.com>
2020-08-24 12:06:45 -0300

    Bean Validation, CSS, Lista de Usuários

--- EpicTask/src/main/java/br/com/fiap/EpicTask/model/User.java
+++ EpicTask/src/main/java/br/com/fiap/EpicTask/model/User.java
@@ -4,15 +4,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity(name = "users")
+//@Data - Lombok - cria todos os Getter´s and Setter´s
 public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank(message = "O nome é obrigatório")
 	private String nome;
+	
+	@NotBlank(message = "O email é obrigatório")
+	@Email(message = "Deve ser um e-mail válido")
 	private String email;
+	  
+	@Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
 	private String pass;
 	
 	public Long getId() {
